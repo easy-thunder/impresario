1434ba6e6569187318b30575d6baa4d60d0abe1b
Jacob David Diehl <jakediehl17@gmail.com>
2023-11-16 20:27:23 -0700

    something insane

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -6,41 +6,49 @@ async function handler(req, res) {
     const password = req.body.password;
     const checkPassword = req.body.checkPassword;
     const hashedPassword = await hashPassword(password);
-
     let client;
-    try {
-        client = await connectToDatabase();
-        const db = client.db();
+    client = await connectToDatabase();
+    const db = client.db();
 
-        if (!email || !email.includes('@') || !password || password.trim().length < 7 || !checkPassword === password) {
-            res.status(422).json({ message: "Invalid input - password should be seven characters long and valid email is needed or your password check failed.", status: 422 });
-            return;
-        }
+    if (!email || !email.includes('@') || !password || password.trim().length < 7 || !checkPassword === password) {
+        res.status(422).json({ message: "Invalid input - password should be seven characters long and valid email is needed or your password check failed.", status: 422 });
+        return;
+    }
 
-        if (req.method === 'POST') {
-            // Check for existing user
+
+
+
+    if (req.method === 'POST') {
+       
+
+      
+
+        try {
+
             const existingUser = await db.collection('users').findOne({ email: email });
 
             if (existingUser) {
                 res.status(422).json({ message: `${existingUser.email} already exists, try logging in` });
-            } else {
-                // Insert new user
-                const result = await db.collection('users').insertOne({
-                    email: email,
-                    password: hashedPassword,
-                    messages: [],
-                });
+                return;
+            }
 
-                res.status(201).json({ message: `${email}` });
+
+
+        } catch {
+            const result = await db.collection('users').insertOne({
+                email: email,
+                password: hashedPassword,
+                messages: [],
+            });
+
+            res.status(201).json({ message: `${email}` });
+            
+        } finally {
+            if (client) {
+                await client.close();
             }
         }
-    } catch (error) {
-        console.error('Error during signup:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    } finally {
-        if (client) {
-            await client.close();
-        }
+        return;
     }
 }
 
